85ee99efa5bbff341e0bc7ae8ea7aaa31c0fb508
David <rr.david.marlor@gmail.com>
2023-08-12 13:35:11 +0200

    Initial structure

---
+++ main.py
@@ -0,0 +1,4 @@
+#Fichero main con la estructura para la visualizacion de la aplicacion
+#Necesito una ventana principal para seleccionar la coleccion que cambiara de ventana y nos mostrara en cuadricula
+#todas las cartas con un check abajo si lo tenemos o no, esto sera leyendo de un fichero en local que tendra el si o no.
+
