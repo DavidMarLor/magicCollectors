b0e306ebad95687f6abb866e71b011c90c6d63ac
David <rr.david.marlor@gmail.com>
2023-08-20 20:14:49 +0200

    Phase 0.2 Quedan cartas por incluir y definir el resto de funciones necesarias

--- modificarColeccion.py
+++ modificarColeccion.py
@@ -24,10 +24,8 @@ def obtenerInfo(nombreSeleccionado):
         for linea in fichero:
             elemento = linea.split(',')
             nombre = elemento[0]
-            print(elemento[1])
             imagen = cv2.imread(elemento[1])
 
-            check = elemento[2]
             if nombreSeleccionado == nombre:
                 print("Encontrada")
                 cv2.imshow('Bill el poni', imagen)
@@ -36,8 +34,26 @@ def obtenerInfo(nombreSeleccionado):
             else:
                 print("No encontrada")
 
-
+def numeroCartas():
+    contador = 0
+    contadorTotal = 0
+    contadorRestantes = 0
+    with open('list.txt', 'r') as fichero:
+        for linea in fichero:
+            elemento = linea.split(',')
+            check = elemento[2].strip()
+            if check == "True":
+                contador += 1
+            if check == "False":
+                contadorRestantes += 1
+            contadorTotal += 1
+    print("El numero de cartas total es: ", contadorTotal, " y el numero de cartas que tengo es: ", contador,
+          " y me quedan: ", contadorRestantes)
 
 #Obtener Imagen, sobre en el que se obtiene y precio
 nombreCarta = "Bill el poni"
-obtenerInfo(nombreCarta)
+contador = 0
+contadorTotal = 0
+contadorRestantes = 0
+#obtenerInfo(nombreCarta)
+numeroCartas()
